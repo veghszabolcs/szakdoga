c8797841320664376ba0e0b4d61c55da8a84bbd7
veghszabolcs <vegh.szabolcs.2019i@bankitatabanya.hu>
2024-01-06 16:09:27 +0100

    hibakezelés

--- register/script.js
+++ register/script.js
@@ -14,3 +14,63 @@ function showPasswordSecond(){
         pw.type = "password";
     }
 }
+function validation(){
+    if(!emailCheck()){
+        document.getElementById("emailError").style.display = "block";
+        return false;
+    }else{
+        document.getElementById("emailError").style.display = "none";
+    }
+    if(!passwordCheckLength()){
+        document.getElementById("passwordLengthError").style.display = "block";
+        return false;
+    }else{
+        document.getElementById("passwordLengthError").style.display = "none";
+    }
+    if(!passwordCheckCharacters()){
+        document.getElementById("passwordCharacterError").style.display = "block";
+        return false;
+    }else{
+        document.getElementById("passwordCharacterError").style.display = "none";
+    }
+    return true;
+}
+function emailCheck(){
+    var email = document.getElementById("loginEmail").value;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailRegex.test(email)){
+        return false;
+    }
+    
+    return true;
+}
+function passwordCheckLength(){
+    var firstPw = document.getElementById("loginPassword").value;
+    var secondPw = document.getElementById("loginPasswordSecond").value;
+
+    if(firstPw.length<7 || secondPw.length<7){
+        return false;
+    }
+    
+    return true;
+}
+function passwordCheckCharacters(){
+    var firstPw = document.getElementById("loginPassword").value;
+    var secondPw = document.getElementById("loginPasswordSecond").value;
+
+    if(!containsUpperCase(firstPw) || !containsUpperCase(secondPw)){
+        return false;
+    }
+    if(!containsNumber(firstPw) || !containsNumber(secondPw)){
+        return false;
+    }
+
+    return true;
+}
+function containsUpperCase(str){
+    return /[A-Z]/.test(str);
+}
+function containsNumber(str){
+    return /[0-9]/.test(str);
+}
